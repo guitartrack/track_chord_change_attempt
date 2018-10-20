fb76f215b9ce6e38734d43e86b1ef894be781541
threatclimber <35009941+threatclimber@users.noreply.github.com>
2018-10-20 18:32:20 +0200

    Reduced complexity by moving everything into one file

--- track_chord_change_attempt/app.py
+++ track_chord_change_attempt/app.py
@@ -1,4 +1,20 @@
-from change_attempt import change_attempt
+from datetime import datetime
+
+from pynamodb.models import Model
+from pynamodb.attributes import (
+    NumberAttribute, UnicodeAttribute, UTCDateTimeAttribute)
+
+
+class ChordChangeAttempt(Model):
+    class Meta:
+        table_name = "chord_change_attempt"
+        region = 'eu-central-1'
+
+    chord_change = UnicodeAttribute(hash_key=True)
+    created_at = UTCDateTimeAttribute(default=datetime.utcnow, range_key=True)
+
+    count = NumberAttribute()
+
 
 def lambda_handler(event, context):
     change_attempt(
@@ -6,3 +22,9 @@ def lambda_handler(event, context):
         event['payload']['to_chord'],
         event['payload']['count']
     )
+
+
+def change_attempt(from_chord: str, to_chord: str, count: int):
+    attempt = ChordChangeAttempt(
+        chord_change='{}:{}'.format(from_chord, to_chord), count=count)
+    attempt.save()
